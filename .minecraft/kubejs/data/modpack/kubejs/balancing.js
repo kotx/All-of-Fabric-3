f6cca72860bbe6776e46d675a8828621f801e5ce
AK9 <anthonkrisat@hotmail.com>
2020-09-01 21:30:11 +0200

    changed angel ring & boosters recipe

--- .minecraft/kubejs/data/modpack/kubejs/balancing.js
+++ .minecraft/kubejs/data/modpack/kubejs/balancing.js
@@ -1,4 +1,27 @@
 events.listen("recipes", function (event) {
 
+  // Heart of the Sky
   event.remove({ output: "winged:heart_of_the_sky" });
+
+  // Angel Ring
+  event.remove({ output: "kibe:angel_ring" });
+  event.shaped(item.of("kibe:angel_ring"), [
+    ["dml-refabricated:glitch_ingot",  "astromine:univite_block",         "dml-refabricated:glitch_ingot"],
+    ["techreborn:quantum_solar_panel", "kibe:diamond_ring",               "techreborn:quantum_solar_panel"],
+    ["minecraft:elytra",               "iron-jetpacks:netherite_jetpack", "minecraft:elytra"],
+  ]);
+
+  // Boosters
+  event.remove({ output: "winged:booster_empty" });
+  event.shaped(item.of("winged:booster_empty"), [
+    [null,                     "techreborn:steel_ingot", null],
+    ["techreborn:steel_ingot", "techreborn:cell",        "techreborn:steel_ingot"],
+    ["techreborn:steel_ingot", "techreborn:cell",        "techreborn:steel_ingot"],
+  ]);
+
+  // Solar Panels
+  event.remove({ id: "techreborn:crafting_table/solar_panel/ultimate_solar_panel_alt" });
+  event.remove({ id: "techreborn:crafting_table/solar_panel/industrial_solar_panel_alt" });
+  event.remove({ id: "techreborn:crafting_table/solar_panel/advanced_solar_panel_alt" });
+  event.remove({ id: "techreborn:crafting_table/solar_panel/advanced_solar_panel_alt" });
 });
