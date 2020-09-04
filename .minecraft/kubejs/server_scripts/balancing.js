cff4192b7c9b87addb735f553225f728b3d58dc6
AK9 <anthonkrisat@hotmail.com>
2020-09-04 03:00:01 +0200

    new quantum suit recipes

--- .minecraft/kubejs/server_scripts/balancing.js
+++ .minecraft/kubejs/server_scripts/balancing.js
@@ -1,31 +1,27 @@
 events.listen("recipes", function (event) {
 
+
   // Heart of the Sky
   event.remove({ output: "winged:heart_of_the_sky" });
+
 
   // Angel Ring
   event.remove({ output: "kibe:angel_ring" });
   event.shaped(item.of("kibe:angel_ring"), [
-    ["dml-refabricated:glitch_ingot",  "astromine:univite_block",         "dml-refabricated:glitch_ingot"],
-    ["minecraft:elytra",               "kibe:diamond_ring",               "minecraft:elytra"],
-    ["techreborn:quantum_solar_panel", "iron-jetpacks:netherite_jetpack", "techreborn:quantum_solar_panel"],
+    ["dml-refabricated:glitch_ingot", "astromine:univite_block",         "dml-refabricated:glitch_ingot"],
+    ["minecraft:elytra",              "techreborn:interdimensional_su",  "minecraft:elytra"],
+    ["winged:booster_empty",          "iron-jetpacks:netherite_jetpack", "winged:booster_empty"],
   ]);
+
 
   // Boosters
   event.remove({ output: "winged:booster_empty" });
   event.shaped(item.of("winged:booster_empty"), [
-    ["techreborn:tungstensteel_plate", "techreborn:steel_ingot", "techreborn:tungstensteel_plate"],
-    ["techreborn:steel_ingot",         "techreborn:cell",        "techreborn:steel_ingot"],
-    ["techreborn:steel_ingot",         "techreborn:cell",        "techreborn:steel_ingot"],
+    ["techreborn:titanium_plate", "techreborn:titanium_plate",         "techreborn:titanium_plate"],
+    ["techreborn:titanium_plate", "techreborn:superconductor_upgrade", "techreborn:titanium_plate"],
+    ["techreborn:titanium_plate", "techreborn:superconductor_upgrade", "techreborn:titanium_plate"],
   ]);
 
-  // Rocket Aglet
-  event.remove({ output: "bunchotrinkets:rocket_aglet" });
-  event.shaped(item.of("bunchotrinkets:rocket_aglet"), [
-    ["astromine:stellum_plate", "astromine:stellum_ingot",     "astromine:stellum_plate"],
-    ["minecraft:firework_rocket",      "blockus:legacy_nether_reactor_core", "minecraft:firework_rocket"],
-    ["astromine:stellum_plate", "astromine:stellum_ingot",     "astromine:stellum_plate"],
-  ]);
 
   // Altar
   event.shaped(item.of("astromine:altar"), [
@@ -34,6 +30,7 @@ events.listen("recipes", function (event) {
     ["techreborn:tungstensteel_storage_block", "techreborn:tungstensteel_storage_block", "techreborn:tungstensteel_storage_block"],
   ]);
 
+
    // Altar Pedestal
    event.shaped(item.of("astromine:altar_pedestal"), [
      ["techreborn:tungstensteel_plate", "techreborn:tungstensteel_plate", "techreborn:tungstensteel_plate"],
@@ -41,11 +38,43 @@ events.listen("recipes", function (event) {
      ["techreborn:tungstensteel_plate", "techreborn:tungstensteel_plate", "techreborn:tungstensteel_plate"],
    ]);
 
+
+    // Quantum Suit
+  event.remove({ output: "techreborn:quantum_helmet" });
+  event.shaped(item.of("techreborn:quantum_helmet"), [
+    ["techreborn:data_storage_chip", "techreborn:lapotronic_orb", "techreborn:data_storage_chip"],
+    ["winged:booster_empty",         null,                        "winged:booster_empty"],
+    [null,                           null,                        null],
+  ]);
+
+  event.remove({ output: "techreborn:quantum_chestplate" });
+  event.shaped(item.of("techreborn:quantum_chestplate"), [
+    ["techreborn:tungstensteel_plate",  null,                                   "techreborn:tungstensteel_plate"],
+    ["techreborn:superconductor_cable", "kibe:angel_ring",                      "techreborn:superconductor_cable"],
+    ["techreborn:data_storage_chip",    "techreborn:iridium_neutron_reflector", "techreborn:data_storage_chip"],
+  ]);
+
+  event.remove({ output: "techreborn:quantum_leggings" });
+  event.shaped(item.of("techreborn:quantum_leggings"), [
+    ["techreborn:data_storage_chip",   "techreborn:lapotronic_orb", "techreborn:data_storage_chip"],
+    ["winged:booster_empty",           null,                        "winged:booster_empty"],
+    ["techreborn:tungstensteel_plate", null,                        "techreborn:tungstensteel_plate"],
+  ]);
+
+  event.remove({ output: "techreborn:quantum_boots" });
+  event.shaped(item.of("techreborn:quantum_boots"), [
+    ["techreborn:lapotronic_orb",    null, "techreborn:lapotronic_orb"],
+    ["techreborn:data_storage_chip", null, "techreborn:data_storage_chip"],
+    ["winged:booster_empty",         null, "winged:booster_empty"],
+  ]);
+
+
   // Solar Panels
   event.remove({ id: "techreborn:crafting_table/solar_panel/ultimate_solar_panel_alt" });
   event.remove({ id: "techreborn:crafting_table/solar_panel/industrial_solar_panel_alt" });
   event.remove({ id: "techreborn:crafting_table/solar_panel/advanced_solar_panel_alt" });
   event.remove({ id: "techreborn:crafting_table/solar_panel/advanced_solar_panel_alt" });
+
 
   // Metals
   event.remove({ id: "indrev:shapeless/steel_dust" });
