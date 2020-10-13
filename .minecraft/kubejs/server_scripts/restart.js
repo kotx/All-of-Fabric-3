23deb9b2d391a7afff2d90a79f76d3ef67053e9f
Monster Zero <dinanj@gmail.com>
2020-10-13 16:22:10 -0400

    remove old

--- .minecraft/kubejs/server_scripts/restart.js
+++
@@ -1,9 +0,0 @@
-events.listen('server.load', event => {
-  event.server.schedule(25 * MINUTE, event.server, callback => {
-    callback.data.tell('Server restarting in 5 minutes!')
-  })
-  
-  event.server.schedule(30 * MINUTE, event.server, callback => {
-    callback.data.runCommand('/stop')
-  })
-});
